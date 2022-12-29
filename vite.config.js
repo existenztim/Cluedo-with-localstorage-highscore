547c46f5c689a20ac5274be8a65970c7e9d740f8
existenztim <tim920129@hotmail.com>
2022-12-29 23:16:19 +0100

    :memo: changed base adress in config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ då ska "base" här nedan vara "/fed22d-js-grundkurs-jenni-wumpus/"
  */
 
 export default defineConfig({
-  base: '/fed22d-js-grundkurs-2-cluedo-existenztim/', // TODO - ändra till ditt repo-namn
+  base: '/Cluedo-with-localstorage-highscore/', // TODO - ändra till ditt repo-namn
   define: {
     'process.env': {},
   },
